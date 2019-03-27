bf1ab2ea32bac47d45af1ee1ccd2c6a6f47d52ce
Ignacio Oyarzabal <ignaoya93@gmail.com>
2019-03-27 18:25:50 +0100

    Added random minions with a loop for going through their turns after the player moves.

--- engine.py
+++ engine.py
@@ -1,6 +1,7 @@
 import tdl
 
-from entity import Entity
+from entity import Entity, get_blocking_entities_at_location
+from game_states import GameStates
 from input_handlers import handle_keys
 from map_utils import GameMap, make_map
 from render_functions import render_all, clear_all
@@ -19,17 +20,20 @@ def main():
     fov_light_walls = True
     fov_radius = 10
 
+    max_monsters_per_room = 3
+
     colors = {
             'darkness': (0, 0, 10),
             'dark_wall': (0,0,60),
             'dark_ground': (30,30,100),
             'light_wall': (130, 110, 50),
             'light_ground': (200, 180, 50),
+            'desaturated_green': (63, 127, 63),
+            'darker_green': (0, 127, 0),
             }
 
-    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', (255,255,255))
-    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), 'G', (0,255,0))
-    entities = [npc, player]
+    player = Entity(0, 0, '@', (255, 255, 255), 'Player', blocks=True)
+    entities = [player]
 
     tdl.set_font('resources/arial10x10.png', greyscale=True, altLayout=True)
 
@@ -37,9 +41,12 @@ def main():
     con = tdl.Console(screen_width, screen_height)
 
     game_map = GameMap(map_width, map_height)
-    make_map(game_map, max_rooms, room_min_size, room_max_size, map_width, map_height, player)
+    make_map(game_map, max_rooms, room_min_size, room_max_size, map_width, map_height, player,
+             entities, max_monsters_per_room, colors)
     
     fov_recompute = True
+
+    game_state = GameStates.PLAYERS_TURN
 
     while not tdl.event.is_window_closed():
         if fov_recompute:
@@ -68,19 +75,33 @@ def main():
         exit = action.get('exit')
         fullscreen = action.get('fullscreen')
 
-        if move:
+        if move and game_state == GameStates.PLAYERS_TURN:
             dx, dy = move
-            next_x = player.x + dx
-            next_y = player.y + dy
-            if game_map.walkable[next_x, next_y]:
-                player.move(dx, dy)
-                fov_recompute = True
+            destination_x = player.x + dx
+            destination_y = player.y + dy
+
+            if game_map.walkable[destination_x, destination_y]:
+                target = get_blocking_entities_at_location(entities, destination_x, destination_y)
+                if target:
+                    print('You kick the ', target.name, 'in the shins, much to its annoyance!')
+                else:
+                    player.move(dx, dy)
+                    fov_recompute = True
+
+                game_stat = GameStates.ENEMY_TURN
 
         if exit:
             return True
 
         if fullscreen:
             tdl.set_fullscreen(not tdl.get_fullscreen())
+
+        if game_stat == GameStates.ENEMY_TURN:
+            for entity in entities:
+                if entity !=player:
+                    print('The', entity.name, 'ponders the meaning of its existence.')
+
+            game_stat = GameStates.PLAYERS_TURN
 
 if __name__ == '__main__':
     main()
